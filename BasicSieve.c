e0a59b9119009ec556a785a04ae66834048f1b91
lydiab23 <95350063+lydiab23@users.noreply.github.com>
2021-12-08 11:02:27 +0100

    Update BasicSieve.c

--- BasicSieve.c
+++ BasicSieve.c
@@ -12,7 +12,7 @@ unsigned long int cputime()
 	return rus.ru_utime.tv_sec * 1000 + rus.ru_utime.tv_usec / 1000;
 }
 
-
+//fonction qui genere un nombre de taille exactement k bits
 int nombre_k_bits(mpz_t n,int b, gmp_randstate_t mon_generateur){
 	mpz_t z_n;
 	mpz_inits(z_n,NULL);
@@ -23,44 +23,49 @@ int nombre_k_bits(mpz_t n,int b, gmp_randstate_t mon_generateur){
 
 }
 
+
 int crible_simple(int b,int k, int t, mpz_t n, gmp_randstate_t mon_generateur, int primes[])
 {
-	int a,i;
-	mpz_t r0,r1,z_n,rop1,x;
-	mpz_inits(r0,r1,z_n,rop1,x,NULL);
-	int isPrime,test;
+	//on genere un nombre de k bits et vérifie qu'il est impair
+    do
+    {
+        nombre_k_bits(n, b, mon_generateur);
+    } while (mpz_divisible_ui_p(n, 2) != 0);
+	
+    int isPrime = 1;
+    do
+    {
+        isPrime = 1;
+        int i;
+        for (i = 1; i < k; i++)
+        {
+        	//tests de divisions de n par les k premiers nombres premiers
+			gmp_printf("n=%Zd \n",n);
+			printf("prime[%d]=%d \n",i,primes[i]);
+			printf("D_%d \n",i);
+            if (mpz_divisible_ui_p(n, primes[i]) != 0)
+            {
+                isPrime = 0;
+                mpz_add_ui(n, n, 2);
+                printf("G \n"); //G = q=q+2 et retour a la ligne 2
+                break;
+            }
+            printf("I \n");//incrémentation
+        }
 
-	do{
-		nombre_k_bits(n,b,mon_generateur);
-		mpz_cdiv_r_ui(r1,n,2);
-	}while(mpz_cmp_ui(r1,0)==0);
-
-
-	do{
-		isPrime=0;
-		
-		do{
-			test=0;
-			for(i=1;i<k;i++){
-				mpz_cdiv_r_ui(r0,n,primes[i]);
-				if(mpz_cmp_ui(r0,0)==0){
-					mpz_add_ui(n,n,2);
-				}
-				else{
-					test=1;
-				}
+        if (isPrime)
+        {
+        	gmp_printf("T[%Zd} \n",n);
+			if(mpz_probab_prime_p(n,t)==0){
+				mpz_add_ui(n,n,2);
 			}
-		}while(test!=1);
+			else{
+				isPrime=1;
+			}
+        }
+    } while (!isPrime);
 
-		if(mpz_probab_prime_p(n,t)==0){
-			mpz_add_ui(n,n,2);
-		}
-		else{
-			isPrime=1;
-		}
-	}while(isPrime!=1);
-
-	return isPrime;
+    return isPrime;
 }
 
 int main(int argc, char * argv[])
@@ -68,12 +73,19 @@ int main(int argc, char * argv[])
 	int r,b,k,t,u;
 	int seed,i,j,rep;
 	mpz_t n,p,q;
-	mpz_inits(n,p,q,NULL);
+	mpz_t N;
+	mpz_inits(n,N,p,q,NULL);
 	seed = time(NULL);
-	b=atoi(argv[1]);
-	k=atoi(argv[2]);
-	t=atoi(argv[3]);
-	r=atoi(argv[4]);
+	//ce programme genere 2 nombres : p et q 
+	r=2;
+	//de taille exactement 512 bits chacun
+	b=512;
+	//non divisible par les k premiers nombres premiers
+	//on aura trois cas à étudier (T1, k = 54), (T2, k = 60), (T3, k = 70)
+	k=atoi(argv[1]);
+	//avant d’etre testé par la méthode de Miller-Rabin avec t bases
+	t=atoi(argv[2]);
+	
 
 	int primes[k];
 
@@ -88,17 +100,22 @@ int main(int argc, char * argv[])
 	gmp_randinit_default(mon_generateur); //Initialisation du génératuer
 	gmp_randseed_ui(mon_generateur, seed);
 
+	
+	mpz_set_ui(N,1);
 	long int starttime = cputime();
 	for(j=0;j<r;j++){
 		rep=crible_simple(b,k,t,n,mon_generateur,primes);
 		if(rep==1){
 			gmp_printf("n%d = %Zd \n",j,n);
+			printf("\n");
+			mpz_mul(N,N,n);
 		}
 	}
+	gmp_printf("N = %Zd \n",N);
 	long int endtime = cputime();
 	printf("temps d'execution : %ld milli-secondes \n", endtime - starttime);
 
 	gmp_randclear(mon_generateur);
-    mpz_clears(n,p,q, NULL);
+    mpz_clears(n,N,p,q, NULL);
 
 }
